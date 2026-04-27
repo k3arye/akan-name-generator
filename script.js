b70b36b5afef5ff76194654535d9a3848eb041d6
esther <estakearye@gmail.com>
2026-04-27 20:20:27 +0300

    fix: correct spelling of "Generator" in title and add missing newline in script.js

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 document.getElementById("akan-form").addEventListener("click", (event)=> {
 event.preventDefault();
 
+
+let birthday = document.getElementById("birthay").value;
+let genderInput = document.querySelector('input[name="gender"]:checked');
+
 })
     
