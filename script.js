db6df7436c500f265a55bb617fd9498c9dd6eb90
esther <estakearye@gmail.com>
2026-04-27 20:28:03 +0300

    fix: validate birthday and gender input in form submission

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ event.preventDefault();
 let birthday = document.getElementById("birthay").value;
 let genderInput = document.querySelector('input[name="gender"]:checked');
 
+if (!birthday){
+    document.getElementById("akan-name").innerText = "Please enter your birthday!";
+    return;
+}
+if (!genderInput){
+    document.getElementById("akan-name").innerText = "Please select your gender!";
+    return;
+}
+
 })
     
