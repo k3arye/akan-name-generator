4d15197eb4180557d0035921837cc555a28008ec
esther <estakearye@gmail.com>
2026-04-27 20:47:39 +0300

    fix: correct date calculation logic for Akan name generation

--- script.js
+++ script.js
@@ -15,11 +15,25 @@ if (!genderInput){
     return;
 }
 
+let date = new Date(birthday);
 
+let DD = date.getDate();
+let MM = date.getMonth() + 1;
+let YYYY = date.getFullYear();
+
+let CC = Math.floor(YYYY / 100);
+let YY = YYYY % 100;
+let d = (4 * CC - 2 * CC - 1 + 45 * YY + 1026 * (MM + 1) + DD) % 7;
+
+d = (d + 7) % 7;
 
 // Akan Names
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+
+
 
 let akanName;
 if (gender === "male"){
